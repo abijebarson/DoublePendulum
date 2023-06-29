924489aa0471f965566bd5bdebb01275572289d7
abijebarson <57089155+abijebarson@users.noreply.github.com>
2023-06-29 16:40:22 +0530

    Double Pendulum x 1024

--- dp_1000.py
+++ dp_1000.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
-from matplotlib.animation import FuncAnimation 
+from matplotlib.animation import FuncAnimation
 import os.path
 
 BASE = (0,0)
